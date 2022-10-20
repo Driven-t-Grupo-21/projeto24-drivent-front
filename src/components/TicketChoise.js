4094cad97c1dade68e12da4f73e377e58c210a6b
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-10-20 20:56:31 -0300

    not enrolled warning

--- src/components/TicketChoise.js
+++ src/components/TicketChoise.js
@@ -1,31 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import styled from 'styled-components';
 
 import DashboardTitle from './DashboardTitle';
 import TicketCards from './TicketCards';
 import EventInfoContext from '../contexts/EventInfoContext';
+import useToken from '../hooks/useToken';
+import * as enrollmentApi from '../services/enrollmentApi';
+
+let isEnrolled = false;
 
 const TicketChoise = () => {
   const { eventInfo } = useContext(EventInfoContext);
+  const token = useToken();
+  let req;
 
-  console.log(eventInfo);
+  useEffect(() => {
+    req = enrollmentApi.getPersonalInformations(token);
 
-  return (
-    <Container>
+    req.then((response) => {
+      if (response.id !== null) {
+        isEnrolled = true;
+      }
+    });
+    req.catch((err) => {
+      console.log(err);
+    });
+  });
+
+  return (<> {(isEnrolled) ?
+    (
+      <Container>
+        <DashboardTitle>Ingresso e pagamento</DashboardTitle>
+        <h6>Primeiro, escolha sua modalidade de ingresso</h6>
+        <section className="cardsSection">
+          {eventInfo.Ticket.map((ticket, index) => (
+            <>
+              <TicketCards isActive={false} key={index}>
+                <p>{ticket.type}</p>
+                <p className="price">R$ {ticket.price}</p>
+              </TicketCards>
+            </>
+          ))}
+        </section>
+      </Container>
+    ) : (<>
       <DashboardTitle>Ingresso e pagamento</DashboardTitle>
-      <h6>Primeiro, escolha sua modalidade de ingresso</h6>
-      <section className="cardsSection">
-        {eventInfo.Ticket.map((ticket, index) => (
-          <>
-            <TicketCards isActive={false} key={index}>
-              <p>{ticket.type}</p>
-              <p className="price">R$ {ticket.price}</p>
-            </TicketCards>
-          </>
-        ))}
-      </section>
-    </Container>
-  );
+      <Warning><h1>Você precisa completar sua inscrição antes <br/>
+      de prosseguir pra escolha de ingresso</h1></Warning>
+    </>)
+  }
+  </>);
 };
 
 export default TicketChoise;
@@ -40,4 +64,14 @@ const Container = styled.div`
     opacity: 0.5;
     font-size: 20px;
   }
+`;
+
+const Warning = styled.div`
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #8E8E8E;
+  text-align: center;
+  font-size: 20px;
 `;
