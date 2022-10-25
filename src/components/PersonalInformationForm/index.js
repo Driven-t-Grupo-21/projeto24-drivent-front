b0c08ad5cfb3323f088607bd5abeaefd4f577c2d
MattGueler <mateus.gueler@gmail.com>
2022-10-25 15:16:03 -0300

    :hammer: fix:
    Solves problems when age is under 18

--- src/components/PersonalInformationForm/index.js
+++ src/components/PersonalInformationForm/index.js
@@ -31,17 +31,14 @@ export default function PersonalInformationForm() {
   const { enrollment } = useEnrollment();
   const { saveEnrollmentLoading, saveEnrollment } = useSaveEnrollment();
 
-  const {
-    handleSubmit,
-    handleChange,
-    data,
-    errors,
-    setData,
-    customHandleChange,
-  } = useForm({
+  const { handleSubmit, handleChange, data, errors, setData, customHandleChange } = useForm({
     validations: FormValidations,
 
     onSubmit: async(data) => {
+      if (!data.birthday) {
+        toast('Usuário deve ter mais de 18 anos!');
+        return;
+      }
       const newData = {
         name: data.name,
         cpf: data.cpf.replaceAll('.', '').replaceAll('-', ''),
@@ -94,7 +91,7 @@ export default function PersonalInformationForm() {
         number: enrollment.address.number,
         state: enrollment.address.state,
         neighborhood: enrollment.address.neighborhood,
-        addressDetail: enrollment.address.addressDetail
+        addressDetail: enrollment.address.addressDetail,
       });
     }
   }, [enrollment]);
@@ -111,7 +108,7 @@ export default function PersonalInformationForm() {
     if (isValidCep(valueWithoutMask)) {
       const newDataValues = {
         ...data,
-        [name]: value
+        [name]: value,
       };
 
       setDynamicInputIsLoading(true);
@@ -126,7 +123,7 @@ export default function PersonalInformationForm() {
         state: cepData.uf,
       });
     }
-  };
+  }
 
   return (
     <>
@@ -195,13 +192,7 @@ export default function PersonalInformationForm() {
             {errors.cep && <ErrorMsg>{errors.cep}</ErrorMsg>}
           </InputWrapper>
           <InputWrapper>
-            <Select
-              label="Estado"
-              name="state"
-              id="state"
-              value={data?.state || ''}
-              onChange={handleChange('state')}
-            >
+            <Select label="Estado" name="state" id="state" value={data?.state || ''} onChange={handleChange('state')}>
               <MenuItem value="">
                 <em>None</em>
               </MenuItem>
@@ -236,12 +227,7 @@ export default function PersonalInformationForm() {
           </InputWrapper>
 
           <InputWrapper>
-            <Input
-              label="Número"
-              name="number"
-              value={data?.number || ''}
-              onChange={handleChange('number')}
-            />
+            <Input label="Número" name="number" value={data?.number || ''} onChange={handleChange('number')} />
             {errors.number && <ErrorMsg>{errors.number}</ErrorMsg>}
           </InputWrapper>
           <InputWrapper>
@@ -262,7 +248,7 @@ export default function PersonalInformationForm() {
               onChange={handleChange('addressDetail')}
             />
           </InputWrapper>
-          
+
           <SubmitContainer>
             <Button type="submit" disabled={dynamicInputIsLoading || saveEnrollmentLoading}>
               Salvar
@@ -275,12 +261,12 @@ export default function PersonalInformationForm() {
 }
 
 const StyledTypography = styled(Typography)`
-  margin-bottom: 20px!important;
+  margin-bottom: 20px !important;
 `;
 
 const SubmitContainer = styled.div`
-  margin-top: 40px!important;
-  width: 100%!important;
+  margin-top: 40px !important;
+  width: 100% !important;
 
   > button {
     margin-top: 0 !important;
